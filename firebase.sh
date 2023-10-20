cd7bccd64d3a81f18d9202ffa63cceb853e0e22f
Heliomar P. Marques <heliomarpm@gmail.com>
2023-10-20 18:51:32 -0300

    add firebase hosting

---
+++ firebase.sh
@@ -0,0 +1,12 @@
+#1. Instalar a Firebase CLI
+npm install -g firebase-tools
+
+#2. Inicializar
+  #2.1 Faça login no Google
+  firebase login
+
+  #2.2 Iniciar seu projeto
+  firebase init
+
+#3. Implantar no Firebase Hosting
+firebase deploy
