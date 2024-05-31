d196f8afd0ee1b9599213f647df316102618adc9
Heliomar P. Marques <heliomarpm@gmail.com>
2024-05-31 18:46:17 -0300

    feat: fix title page and add install instructions

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,18 @@
-import { Component } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { Component } from "@angular/core";
+import { environment } from "../../environments/environment";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  title = `@heliomar/ion-calendar - ${environment.version}`;
+  title = `@heliomarpm/ion-calendar - ${environment.version}`;
 
   constructor() { }
 
   toggleTheme(event: any) {
     const dark = event.detail.checked || false;
-    document.body.setAttribute('color-theme', (dark ? 'dark' : 'light'));
+    document.body.setAttribute("color-theme", dark ? "dark" : "light");
   }
-
 }
